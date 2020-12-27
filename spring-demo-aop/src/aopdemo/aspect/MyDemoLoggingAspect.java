0a882e37a1474e404efa1be0c7b31854fe227959
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-27 20:50:52 +0530

    Initial Project Setup

---
+++ spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,16 @@
+package aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	// this is where we add all of our related advices for logging
+	// lets start with before advice
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n====>>Executing @Before Advice on addAccount()");
+	}
+}
---
+++ spring-demo-aop/src/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,10 @@
+package aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+	public void addAccount() {
+		System.out.println(getClass() + " Doing my DB work :adding an account");
+	}
+}
