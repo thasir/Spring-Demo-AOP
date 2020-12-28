442b93ded69e862d54e116da0b450bfb6284179d
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-28 21:26:38 +0530

    AOP :Pointcut Declarations

--- spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,15 +2,24 @@ package aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+	@Pointcut("execution(* aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+
 	// this is where we add all of our related advices for logging
 	// lets start with before advice
-	@Before("execution(* aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====>>Executing @Before Advice on addAccount()");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n====>>Performing API Analytics");
 	}
 }
