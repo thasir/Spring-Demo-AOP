8a73c21a0d6478b1f9545268bd869cb4554cfb53
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-28 14:43:36 +0530

    match only DAO addAccount

--- spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 	// this is where we add all of our related advices for logging
 	// lets start with before advice
-	@Before("execution(public void addAccount())")
+	@Before("execution(public void aopdemo.dao.AccountDAO.addAccount())")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====>>Executing @Before Advice on addAccount()");
 	}
