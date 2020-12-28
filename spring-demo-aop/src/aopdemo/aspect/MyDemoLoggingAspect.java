81e0eb68e32c936f66aee835fa6478f966ea44c1
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-28 14:57:26 +0530

    match any return type

--- spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 	// this is where we add all of our related advices for logging
 	// lets start with before advice
-	@Before("execution(public void add*())")
+	@Before("execution(* add*())")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====>>Executing @Before Advice on addAccount()");
 	}
