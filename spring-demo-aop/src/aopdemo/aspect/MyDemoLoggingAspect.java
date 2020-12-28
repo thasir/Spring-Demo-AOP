065e464ebc22b6c83446ef3df58d69020b108567
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-28 21:48:35 +0530

    AOP:Combining Pointcuts excluding getters and setters

--- spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,16 +9,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 	@Pointcut("execution(* aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
+	private void forDaoPackage() {
+	}
+
+	// create pointcut for getter methods
+	@Pointcut("execution(* aopdemo.dao.*.get*(..))")
+	private void getter() {
+	}
+
+	// create pointcut for setter methods
+	@Pointcut("execution(* aopdemo.dao.*.set*(..))")
+	private void setter() {
+	}
+
+	// create pointcut including package...excluding getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {
+	}
 
 	// this is where we add all of our related advices for logging
 	// lets start with before advice
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====>>Executing @Before Advice on addAccount()");
 	}
-	
-	@Before("forDaoPackage()")
+
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 		System.out.println("\n====>>Performing API Analytics");
 	}
